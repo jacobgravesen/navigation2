76b2db4b4e6fc50aa8bca94212c1d8ecf3c998df
William Tolstrup <williamtolstrup@hotmail.com>
2022-04-13 09:29:19 +0200

    outcommented install directories from CMakeLists

--- pc/launch/multi_launch.py
+++ pc/launch/multi_launch.py
@@ -49,7 +49,7 @@ def generate_launch_description():
     launch_dir = os.path.join(bringup_dir, 'launch')
 
     # Names and poses of the robots
-    robots = generate_robot_list(5)
+    robots = generate_robot_list(3)
 
     # On this example all robots are launched with the same settings
     autostart = LaunchConfiguration('autostart')
